eeb5f537d089ba9c133d5a279ee1ec70971426f5
sckm <scal.che@gmail.com>
2015-08-25 09:04:15 +0900

    init

---
+++ app/src/develop/java/jp/gr/procon/proconapp/ProconApplication.java
@@ -0,0 +1,6 @@
+package jp.gr.procon.proconapp;
+
+import jp.gr.procon.proconapp.BaseApplication;
+
+public class ProconApplication extends BaseApplication {
+}
---
+++ app/src/main/java/jp/gr/procon/proconapp/BaseApplication.java
@@ -0,0 +1,15 @@
+package jp.gr.procon.proconapp;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(BuildConfig.DEBUG){
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
